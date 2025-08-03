e0a3344940980ab2ee0be7c9bd513e0b8228bf17
Andrea Gandino <andreagandino@gmail.com>
2025-08-03 20:23:21 +0200

    Refactor MoneyAmountGreaterThanZero validation to use Numbers::normalize for value formatting

--- src/Rules/MoneyAmountGreaterThanZero.php
+++ src/Rules/MoneyAmountGreaterThanZero.php
@@ -2,6 +2,7 @@
 
 namespace Andreg\Essentials\Rules;
 
+use Andreg\Essentials\Support\Numbers;
 use Closure;
 use Illuminate\Contracts\Validation\ValidationRule;
 
@@ -11,15 +12,9 @@ class MoneyAmountGreaterThanZero implements ValidationRule {
 	 * Run the validation rule.
 	 */
 	public function validate( string $attribute, mixed $value, Closure $fail ): void {
-		$value = str_replace( ',', '.', $value );
+		$value = Numbers::normalize( strval( $value ) );
 
-		if ( substr_count( $value, '.' ) > 1 ) {
-			$lastDotPos = strrpos( $value, '.' );
-			$value      = str_replace( '.', '', substr( $value, 0, $lastDotPos ) ) . substr( $value, $lastDotPos );
-		}
-
-		// Ensure the value is in a format that can be converted to float
-		if ( floatval( $value ) <= 0 ) {
+		if ( $value <= 0 ) {
 			$fail( 'The :attribute must be greater than 0.' );
 		}
 	}
