d3e72ace66a5c1fea68240d2ba2b5b4b08a0bf09
Andrea Gandino <andreagandino@gmail.com>
2025-08-08 10:31:24 +0200

    Update
    Signed-off-by: Andrea Gandino <andreagandino@gmail.com>

--- tests/Feature/NumbersTest.php
+++ tests/Feature/NumbersTest.php
@@ -3,32 +3,28 @@
 use Andreg\Essentials\Support\Numbers;
 
 describe( 'Numbers', function () {
-	describe( 'normalize', function () {
-		test( 'string numbers can be normalized to float', function () {
-			$amount1 = '123.456,78';
-			$amount2 = '123,456.78';
+	test( 'string numbers can be normalized to float', function () {
+		$amount1 = '123.456,78';
+		$amount2 = '123,456.78';
 
-			expect( Numbers::normalize( $amount1 ) )
-				->toBe( Numbers::normalize( $amount2 ) )
-				->toBe( 123456.78 );
-		} );
+		expect( Numbers::normalize( $amount1 ) )
+			->toBe( Numbers::normalize( $amount2 ) )
+			->toBe( 123456.78 );
 	} );
 
-	describe( 'format', function () {
-		test( 'float numbers can be formatted according to the locale', function () {
-			$amount = 123456.78;
+	test( 'float numbers can be formatted according to the locale', function () {
+		$amount = 123456.78;
 
-			expect( Numbers::format( $amount, 'en' ) )
-				->toBe( '123,456.78' );
+		expect( Numbers::format( $amount, 'en' ) )
+			->toBe( '123,456.78' );
 
-			expect( Numbers::format( $amount, 'it' ) )
-				->toBe( '123.456,78' );
+		expect( Numbers::format( $amount, 'it' ) )
+			->toBe( '123.456,78' );
 
-			expect( Numbers::format( $amount, 'fr' ) )
-				->toBe( "123\u{202F}456,78" );
+		expect( Numbers::format( $amount, 'fr' ) )
+			->toBe( "123\u{202F}456,78" );
 
-			expect( Numbers::format( $amount, 'de' ) )
-				->toBe( '123.456,78' );
-		} );
+		expect( Numbers::format( $amount, 'de' ) )
+			->toBe( '123.456,78' );
 	} );
 } );
